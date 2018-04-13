5230b011ac8af8a87b725923bb0fb7335329f366
Tanner Storment <Tanner.Storment@accuweather.com>
2018-04-13 10:34:08 -0500

    init

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,8 +7,15 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar top &
-polybar bottom &
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload top &
+    MONITOR=$m polybar --reload bottom &
+  done
+else
+  polybar --reload top &
+  polybar --reload bottom &
+fi
 
 
 echo "Bars launched..."
